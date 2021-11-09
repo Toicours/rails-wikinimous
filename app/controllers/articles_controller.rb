3622427b720b1c406754a506ec9eb72db0c7bade
Mathieu Courtois <kourtois@protonmail.com>
2021-11-09 18:54:56 +0100

    crud done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,22 +6,31 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
   def create
     @article = Article.new(task_params)
     @article.save
     redirect_to article_path(@article)
   end
 
-  def show
-  end
-
   def edit
   end
 
   def update
+    @article.update(task_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
 
